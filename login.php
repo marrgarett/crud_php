2fcdd21d296cb341f493d93decebb642afa3dd92
marrgarett <sb6560259101@lru.ac.th>
2023-12-08 10:52:01 +0700

    8dec23

--- login.php
+++ login.php
@@ -1,14 +1,16 @@
 <?php
+  session_start();
   include_once('connect_db.php');
   if (isset($_POST['login'])) {
     $sql = "SELECT * FROM register WHERE email = ? AND pass_word = ?";
     $email = $_POST['email'];
-    $pass_word = md5($_POST['password']);
+    $pass_word = md5($_POST['pass_word']);
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('ss', $email, $pass_word);
     $stmt->execute();
     $result = $stmt->get_result();
     $row = $result->fetch_assoc();
+    $_SESSION['email'] = $row['email'];
     if ($row > 0 ) {
       echo '<script type="text/javascript">';
       echo 'setTimeout(function () { swal.fire({
@@ -49,6 +51,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+
+    <script src="sweetalert2/dist/sweetalert2.min.js"></script>
+    <link rel="stylesheet" href="sweetalert2/dist/sweetalert2.min.css">
+
   </head>
   <body>
     <div class="center">
@@ -82,5 +88,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
   </body>
 </html>
