8872b054361336c2fc90667d1693208c9c01c5c3
marrgarett <sb6560259101@lru.ac.th>
2023-12-15 10:24:53 +0700

    15dec23

--- registerDel.php
+++ registerDel.php
@@ -5,9 +5,9 @@ $id = $_GET["id"];
 $sql = "DELETE FROM register WHERE `register`.`id` = $id";
 
 if (mysqli_query($conn, $sql)) {
-    echo '<script language="JavaScript">';
-    echo 'alert("ลบข้อมูลเรียบร้อยแล้ว"); location.href = "register.php"';
-    echo '</script>';
+  echo '<script language="JavaScript">';
+  echo 'alert("ลบข้อมูลเรียบร้อยแล้ว"); location.href = "register.php"';
+  echo '</script>';
 } else {
   echo "Error deleting record: " . mysqli_error($conn);
 }
